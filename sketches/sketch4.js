daca4cdb21a60b429f4016dc4b552c0b30447a64
Charlene Chang <charlenechang@Charlenes-MacBook-Pro-2.local>
2025-10-27 22:39:00 -0700

    setup racetrack background

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -1,14 +1,27 @@
 // Instance-mode sketch for tab 4
 registerSketch('sk4', function (p) {
+  // Setup racetrack variables
+  let startLine = 0;
+  let finishLine = p.windowWidth - 100;
+
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
   p.draw = function () {
-    p.background(200, 240, 200);
-    p.fill(30, 120, 40);
-    p.textSize(32);
+    p.background(121, 96, 76);
+    
+    // Draw start line
+    p.noStroke();
+    p.fill(0);
+    p.rect(startLine, 0, 5, p.height);
+    
+    // Draw finish line
+    p.fill(0, 255, 0);
+    p.rect(finishLine, 0, 20, p.height);
+
+    p.textSize(22);
     p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. C', p.width / 2, p.height / 2);
+    p.text('Goal', p.width /1.2, p.height / 5);
   };
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
